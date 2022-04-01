576efb19bad4f59ae1ef00b470e0d130efdf9ec7
Skyiros <sherst155@mail.ru>
2022-04-02 00:33:04 +0300

    Fixed bugs

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
-val totalTransferred: Int = 0
-var lastTransfer: Int = 0
-
+var totalTransferredVK: Int = 0
+var totalTransferredOtherCard: Int = 0
+var lastTransferVK: Int = 0
+var lastTransferOtherCard: Int = 0
 
 fun main() {
     val typeCardVK = "VKPay"
@@ -9,17 +10,19 @@ fun main() {
     val typeCardMir = "Mir"
     val typeCardVisa = "Visa"
 
+    val transfer1 = 10_000
+    val transfer2 = 600_000
+    val transfer3 = 8_000_000
+    val transfer4 = 20_000_000
 
-    val transfer1: Int = 10_000
-    val transfer2: Int = 600_000
-    val transfer3: Int = 8_000_000
-
-    println(calculateCommission(amountTransfer = transfer1,lastTransfer = lastTransfer))
-    println(calculateCommission(typeCardMir, transfer2, lastTransfer))
-    println(calculateCommission(typeCardMastercard, transfer3, lastTransfer))
+    println(calculateCommission(amountTransfer = transfer1,lastTransfer = lastTransferVK))
+    println(calculateCommission(typeCardMir, transfer2, lastTransferOtherCard))
+    println(calculateCommission(typeCardMastercard, transfer3, lastTransferOtherCard))
+    println(calculateCommission(typeCardVK, transfer4, lastTransferVK))
 }
 
 fun calculateCommission(typeCard: String = "VKPay", amountTransfer: Int, lastTransfer: Int): String {
+    println("Сумма перевода составляет: ${showKopecksToString(amountTransfer)}")
     if (checkTheLimit(amountTransfer, typeCard)) return ""
     val commission = when(typeCard) {
         "Mastercard","Maestro" -> showKopecksToString(calculateCommissionMastercardMaestro(amountTransfer))
@@ -27,7 +30,7 @@ fun calculateCommission(typeCard: String = "VKPay", amountTransfer: Int, lastTra
         "VKPay" -> showKopecksToString(calculateCommissionVKPay(amountTransfer))
         else -> "Введенная карта на поддерживается!"
     }
-    return "Комиссия в копейках составляет: $commission"
+    return "Комиссия в копейках составляет: $commission \n"
 }
 
 fun checkTheLimit(count: Int, typeCard: String): Boolean {
@@ -39,10 +42,10 @@ fun checkTheLimit(count: Int, typeCard: String): Boolean {
         return true
     }
 
-    if (typeCard == "VKPay" && totalTransferred > 4_000_000) {
+    if (typeCard == "VKPay" && totalTransferredVK > 4_000_000) {
         println("Превышен максимум переводов за месяц!")
         return true
-    } else if (totalTransferred > 60_000_000){
+    } else if (totalTransferredOtherCard > 60_000_000){
         println("Превышен максимум переводов за месяц!")
         return true
     }
@@ -51,16 +54,22 @@ fun checkTheLimit(count: Int, typeCard: String): Boolean {
 }
 
 fun calculateCommissionVisaMir(count: Int): Int {
+    lastTransferOtherCard = count
+    totalTransferredOtherCard += count
     val minimalCommission = 3_500
     val commission = (count * 75 / 10_000)
     return if (commission > minimalCommission) commission else minimalCommission
 }
 
 fun calculateCommissionMastercardMaestro(count: Int): Int {
+    lastTransferOtherCard = count
+    totalTransferredOtherCard += count
     return if (count in 30_000..7_500_000) 0 else (count * 6 / 1_000) + 20
 }
 
 fun calculateCommissionVKPay(count: Int): Int {
+    lastTransferVK = count
+    totalTransferredVK += count
     return 0
 }
 
