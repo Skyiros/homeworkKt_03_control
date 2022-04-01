0310f13f9d369388983fcc188115fa214d70cbb7
Skyiros <sherst155@mail.ru>
2022-04-01 21:47:55 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,20 @@
+fun main() {
+//    val minutesMode = "toMinutes"
+//    val secondsMode = "toSeconds"
+    val user1Time = 5
+    val user2Time = 423
+    val user3Time = 60000
+    val user4Time = 100000
+    val user5Time = 210000
+    val user6Time = 999999
+
+//    println(countTimeDisconnect(user1Time))
+//    println(countTimeDisconnect(user2Time))
+//    println(countTimeDisconnect(user3Time))
+//    println(countTimeDisconnect(user4Time))
+//    println(countTimeDisconnect(user5Time))
+//    println(countTimeDisconnect(user6Time))
+}
+
+fun countTimeDisconnect(timeCount: Int) = when {
+}
